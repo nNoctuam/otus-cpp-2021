2b131221d8d3b04d89d8629e2bf30b852122326f
nNoctuam <nnoctuam@gmail.com>
2021-05-10 22:29:27 +0300

    Tuple

--- main.cpp
+++ main.cpp
@@ -21,10 +21,13 @@ struct is_string<T, std::void_t<typename is_std_container<T>::type,
                                 decltype(std::declval<T &>().c_str())>>
     : std::true_type {};
 
+template <typename> struct is_tuple : std::false_type {};
+
+template <typename... T> struct is_tuple<std::tuple<T...>> : std::true_type {};
+
 template <typename BytesT>
 typename std::enable_if<std::is_convertible<BytesT, int>::value, BytesT>::type
 print_ip(BytesT ip) {
-  std::cout << __PRETTY_FUNCTION__ << std::endl;
   for (unsigned long i = sizeof(BytesT); i > 1; i--) {
     std::cout << (uint) * (reinterpret_cast<uint8_t *>(&ip) + i - 1) << '.';
   }
@@ -37,7 +40,6 @@ typename std::enable_if<is_std_container<ContainerT>::value &&
                             !is_string<ContainerT>::value,
                         ContainerT>::type
 print_ip(ContainerT ip) {
-  std::cout << __PRETTY_FUNCTION__ << std::endl;
   if (ip.begin() != ip.end()) {
     std::cout << std::to_string(*ip.begin());
   }
@@ -56,13 +58,35 @@ typename std::enable_if<is_std_container<StringT>::value &&
                             is_string<StringT>::value,
                         StringT>::type
 print_ip(StringT ip) {
-  std::cout << __PRETTY_FUNCTION__ << std::endl;
   std::cout << ip << std::endl;
   return ip;
 }
 
-int main(int, char **) {
+template <int SIZE, int RPOS, typename TupleT>
+typename std::enable_if<RPOS == 0, TupleT>::type
+print_tuple_elements(const TupleT &ip) {
+  return ip;
+}
 
+template <int SIZE, int RPOS, typename TupleT>
+typename std::enable_if<is_tuple<TupleT>::value && RPOS != 0, TupleT>::type
+print_tuple_elements(const TupleT &ip) {
+  std::cout << '.' << std::get<SIZE - RPOS>(ip);
+  print_tuple_elements<SIZE, RPOS - 1, TupleT>(ip);
+  return ip;
+}
+
+template <typename TupleT>
+typename std::enable_if<is_tuple<TupleT>::value, TupleT>::type
+print_ip(TupleT ip) {
+  std::cout << std::get<0>(ip);
+  constexpr int size = std::tuple_size<TupleT>();
+  print_tuple_elements<size, size - 1, TupleT>(ip);
+  std::cout << std::endl;
+  return ip;
+}
+
+int main(int, char **) {
   print_ip(char{-1});                             // 255
   print_ip(short{0});                             // 0.0
   print_ip(int{2130706433});                      // 127.0.0.1
@@ -70,7 +94,7 @@ int main(int, char **) {
   print_ip(std::string{"Hello, World!"});         // Hello, World!
   print_ip(std::vector<int>{100, 200, 300, 400}); // 100.200.300.400
   print_ip(std::list<short>{400, 300, 200, 100}); // 400.300.200.100
-  //  print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
+  print_ip(std::make_tuple(123, 456, 789, 0));    // 123.456.789.0
 
   return 0;
 }
