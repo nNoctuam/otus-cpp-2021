013b9acffe5cce13c462545971ef8d45b2c9a6bc
nNoctuam <nnoctuam@gmail.com>
2021-07-04 18:44:57 +0300

    Logger, max size argument

--- main.cpp
+++ main.cpp
@@ -2,7 +2,53 @@
 #include "string"
 #include "vector"
 
+#include <fstream>
 #include <iostream>
+#include <chrono>
+
+class Logger {
+public:
+
+  void set_file_name(const std::string &name) {
+    if (_file.is_open()) {
+      _file.flush();
+      _file.close();
+    }
+    _file.open(name, std::ios_base::app);
+  }
+
+  template <typename T>
+  friend Logger& operator << (Logger &l, const T &value) {
+    l._file << value;
+    std::cout << value;
+    return l;
+  }
+
+  friend Logger& operator<< (Logger &l, std::ostream& (*f)(std::ostream &)) {
+    f(std::cout);
+    f(l._file);
+    return l;
+  }
+
+  friend Logger& operator<< (Logger &l, std::ostream& (*f)(std::ios &)) {
+    f(std::cout);
+    f(l._file);
+    return l;
+  }
+
+  friend Logger& operator<< (Logger &l, std::ostream& (*f)(std::ios_base &)) {
+    f(std::cout);
+    f(l._file);
+    return l;
+  }
+
+  ~Logger() {
+    _file.flush();
+    _file.close();
+  }
+private:
+  std::ofstream _file;
+};
 
 class ICommand {
 public:
@@ -12,15 +58,22 @@ public:
 
 class Command : public ICommand {
 public:
-  explicit Command(std::string name) : _name(std::move(name)) {}
-  void run() override { std::cout << _name; }
+  explicit Command(std::string name, std::shared_ptr<Logger> logger)
+      : _name(std::move(name)), _logger(std::move(logger)) {}
+  void run() override {
+    (*_logger) << _name;
+  }
 
 private:
   std::string _name;
+  std::shared_ptr<Logger> _logger;
 };
 
 class Container : public ICommand {
 public:
+  explicit Container(std::shared_ptr<Logger> logger)
+      : _logger(std::move(logger)) {}
+
   void push(ICommand *command) { _commands.push_back(command); }
 
   void run() override {
@@ -28,18 +81,22 @@ public:
       _commands[i]->run();
       _commands[i]->~ICommand();
       if (i < _commands.size() - 1) {
-        std::cout << ", ";
+        (*_logger) << ", ";
       }
     }
     _commands.clear();
   }
 
+protected:
   std::vector<ICommand *> _commands;
+  std::shared_ptr<Logger> _logger;
+  friend class Controller;
 };
 
 class Controller : public Container {
 public:
-  explicit Controller(size_t maxBulkLength) : _max_bulk_length(maxBulkLength) {
+  explicit Controller(size_t maxBulkLength, std::shared_ptr<Logger> logger)
+      : Container(std::move(logger)), _max_bulk_size(maxBulkLength) {
     _call_stack.push_back(this);
   }
 
@@ -51,8 +108,12 @@ public:
     } else if ((cmd.empty() || cmd == "EOF") && at_root()) {
       run();
     } else {
-      _call_stack.at(_call_stack.size() - 1)->push(new Command(cmd));
-      if (at_root() && _commands.size() >= _max_bulk_length) {
+      if (at_root() && _commands.empty()) {
+        const auto p1 = std::time(nullptr);
+        _logger->set_file_name("bulk" + std::to_string(p1) + ".log");
+      }
+      _call_stack.at(_call_stack.size() - 1)->push(new Command(cmd, _logger));
+      if (at_root() && _commands.size() >= _max_bulk_size) {
         run();
       }
     }
@@ -62,9 +123,9 @@ public:
     if (_commands.empty()) {
       return;
     }
-    std::cout << "bulk: ";
+    (*_logger) << "bulk: ";
     Container::run();
-    std::cout << std::endl;
+    (*_logger) << std::endl;
   }
 
 private:
@@ -73,7 +134,7 @@ private:
       run();
     }
 
-    auto cmd = new Container();
+    auto cmd = new Container(_logger);
     _call_stack.at(_call_stack.size() - 1)->_commands.push_back(cmd);
     _call_stack.push_back(cmd);
   }
@@ -89,21 +150,23 @@ private:
 
   bool at_root() const { return _call_stack.size() == 1; }
 
-  size_t _max_bulk_length;
+  size_t _max_bulk_size;
   std::vector<Container *> _call_stack;
 };
 
-// todo read length as argument
-// todo logger
 
-int main(int, char **) {
+ int main(int argc, char * argv[]) {
+    if (argc < 2) {
+      std::cerr << "usage: " << argv[0] << "MAX_BULK_SIZE" << std::endl;
+      return 1;
+    }
+
+    size_t max_bulk_length = atoi(argv[1]);
+  Controller root(max_bulk_length, std::make_shared<Logger>());
+
   std::string cmd;
-  size_t max_bulk_length = 3;
-  //  std::cin >> _max_bulk_length;
-
-  Controller root(max_bulk_length);
-
   std::cin >> cmd;
+
   while (!cmd.empty()) {
     root.handleInput(cmd);
     std::cin >> cmd;
