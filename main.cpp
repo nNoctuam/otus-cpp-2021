0808df4bc3a6590557f3f9867509eb6cb15f20dc
nNoctuam <nnoctuam@gmail.com>
2021-07-04 17:39:19 +0300

    WIP

--- main.cpp
+++ main.cpp
@@ -1,11 +1,115 @@
-#include "lib.h"
+#include "memory"
+#include "string"
+#include "vector"
 
 #include <iostream>
 
-int main (int, char **)
-{
-    std::cout << "build " << version() << std::endl;
-    std::cout << "Hello, World!" << std::endl;
+class ICommand {
+public:
+  virtual void run(){};
+  virtual ~ICommand(){};
+};
 
-    return 0;
+class Command : public ICommand {
+public:
+  explicit Command(std::string name) : _name(std::move(name)) {}
+  void run() override { std::cout << _name; }
+
+private:
+  std::string _name;
+};
+
+class Container : public ICommand {
+public:
+  void push(ICommand *command) { _commands.push_back(command); }
+
+  void run() override {
+    for (size_t i = 0; i < _commands.size(); i++) {
+      _commands[i]->run();
+      _commands[i]->~ICommand();
+      if (i < _commands.size() - 1) {
+        std::cout << ", ";
+      }
+    }
+    _commands.clear();
+  }
+
+  std::vector<ICommand *> _commands;
+};
+
+class Controller : public Container {
+public:
+  explicit Controller(size_t maxBulkLength)
+      : _max_bulk_length(maxBulkLength) {
+    _call_stack.push_back(this);
+  }
+
+  void handleInput(std::string &cmd) {
+    //    std::cout << "handle input: " << cmd << std::endl;
+    if (cmd == "{") {
+      go_deeper();
+    } else if (cmd == "}") {
+      go_upper();
+    } else if ((cmd.empty() || cmd == "EOF") && at_root()) {
+      run();
+    } else {
+      push(new Command(cmd));
+      if (_call_stack.at(_call_stack.size() - 1) == this &&
+          _commands.size() >= _max_bulk_length) {
+        run();
+      }
+    }
+  }
+
+  void run() override {
+    if (_commands.empty()) {
+      return;
+    }
+    std::cout << "bulk: ";
+    Container::run();
+    std::cout << std::endl;
+  }
+
+private:
+  void go_deeper() {
+    if (at_root()) {
+      run();
+    }
+
+    auto cmd = new Container();
+    _call_stack.at(_call_stack.size() - 1)->_commands.push_back(cmd);
+    _call_stack.push_back(cmd);
+  }
+
+  void go_upper() {
+    if (!at_root()) {
+      _call_stack.pop_back();
+    }
+    if (at_root()) {
+      run();
+    }
+  }
+
+  bool at_root() const {
+    return _call_stack.size() == 1;
+  }
+
+  size_t _max_bulk_length;
+  std::vector<Container *> _call_stack;
+};
+
+int main(int, char **) {
+  std::string cmd;
+  size_t max_bulk_length = 3;
+  //  std::cin >> _max_bulk_length;
+
+  Controller root(max_bulk_length);
+
+  std::cin >> cmd;
+  while (!cmd.empty()) {
+    root.handleInput(cmd);
+    std::cin >> cmd;
+  }
+  root.run();
+  return 0;
 }
